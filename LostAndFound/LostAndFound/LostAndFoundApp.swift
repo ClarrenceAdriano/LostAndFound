5f369db766286f6822991c7dd0b7180deb3b1a5f
ClarrenceAdriano <cadriano@student.ciputra.ac.id>
2026-05-31 18:56:11 +0700

    file initiate

---
+++ LostAndFound/LostAndFound/LostAndFoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostAndFoundApp.swift
+//  LostAndFound
+//
+//  Created by Clarrence Adriano Hemeldan on 31/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LostAndFoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

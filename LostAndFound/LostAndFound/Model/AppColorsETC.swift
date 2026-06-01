f6a3ddbf4a8a485099c1dfd413e11e127e0ca302
dylan <dylanpatrickw@gmail.com>
2026-06-01 17:14:54 +0700

    add listing view

---
+++ LostAndFound/LostAndFound/Model/AppColorsETC.swift
@@ -0,0 +1,8 @@
+//
+//  AppColors.swift
+//  LostAndFound
+//
+//  Created by Selena Cheryl Willyam on 01/06/26.
+//
+
+import Foundation
---
+++ LostAndFound/LostAndFound/Views/SharedComponents.swift
@@ -0,0 +1,18 @@
+//
+//  SharedComponents.swift
+//  LostAndFound
+//
+//  Created by Selena Cheryl Willyam on 01/06/26.
+//
+
+import SwiftUI
+
+struct SharedComponents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SharedComponents()
+}
